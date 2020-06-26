5dfa3303663fe960ea99d25cd365efec9c69a5c1
obenno <obennoname@gmail.com>
2020-06-26 14:01:43 +0800

    Add support for tab file without header row

--- convert_to_xlsx.R
+++ convert_to_xlsx.R
@@ -7,11 +7,13 @@ option_list <- list(
     make_option(c("-i", "--input"), type = "character", default=NULL,
                 help="input file [%default]"),
     make_option(c("-o", "--output"), type = "character", default=NULL,
-                help="output file [%default]")
+                help="output file [%default]"),
+    make_option(c("--noheader"), action="store_false", default=TRUE,
+                help="input file has no header")
     )
 
 opt <- parse_args(OptionParser(option_list=option_list))
 
-a <- read.delim(opt$input, header = T, sep="\t")
+a <- read.delim(opt$input, header = opt$noheader, sep="\t")
 write.xlsx2(a, file=opt$output, sheetName="Sheet1", 
-            col.names=TRUE, row.names=FALSE, append=FALSE)
+            col.names= opt$noheader, row.names=FALSE, append=FALSE)
