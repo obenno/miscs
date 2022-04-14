aedade203b0f89af9ad18de96968ccc78362c19c
obenno <obennoname@gmail.com>
2022-04-14 20:20:35 +0800

    Changed backend to writexl package

--- convert_to_xlsx.R
+++ convert_to_xlsx.R
@@ -1,19 +1,34 @@
 #! /usr/bin/Rscript
 
-suppressPackageStartupMessages(library(xlsx))
+suppressPackageStartupMessages(library(writexl))
+suppressPackageStartupMessages(library(readr))
 suppressPackageStartupMessages(library(optparse))
 
 option_list <- list(
     make_option(c("-i", "--input"), type = "character", default=NULL,
-                help="input file [%default]"),
+                help="input file(s), could be comma seperated list [%default]"),
+    make_option(c("-n", "--name"), type = "character", default="Sheet1",
+                help="sheet names, could be comma seperated list [%default]"),
     make_option(c("-o", "--output"), type = "character", default=NULL,
                 help="output file [%default]"),
-    make_option(c("--noheader"), action="store_false", default=TRUE,
-                help="input file has no header")
+    make_option(c("--header"), action="store_false", default=TRUE,
+                help="read_tsv col_names option")
     )
 
 opt <- parse_args(OptionParser(option_list=option_list))
 
-a <- read.delim(opt$input, header = opt$noheader, sep="\t")
-write.xlsx2(a, file=opt$output, sheetName="Sheet1", 
-            col.names= opt$noheader, row.names=FALSE, append=FALSE)
+
+inputFile <- unlist(strsplit(opt$input, ","))
+sheetNames <- unlist(strsplit(opt$name, ","))
+if(length(inputFile) != length(sheetNames)){
+    stop("Please ensure sheet names length match number of inputs.")
+}
+
+d <- list()
+for(i in 1:length(inputFile)){
+    d[[sheetNames[i]]] <- read_tsv(inputFile[i],
+                                   col_names = opt$header)
+}
+
+write_xlsx(d, path=opt$output,
+           col_names= opt$header)
