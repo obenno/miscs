01d662ace5896d89171f73d04f91c7d1f00a0c17
obenno <obennoname@gmail.com>
2022-05-11 14:36:49 +0800

    Added future support for extracting genomics sequence

--- extract_GenomicSeq_fromGTF.R
+++ extract_GenomicSeq_fromGTF.R
@@ -6,7 +6,11 @@ option_list <- list(
     make_option(c("-i", "--input"),
                 help="input GTF file"),
     make_option(c("-g", "--genome"),
-                help = "genome fasta file")
+                help = "genome fasta file"),
+    make_option(c("--outfmt"), type="character", default = "fasta",
+                help = "output result format, tabular or fasta"),
+    make_option(c("-t", "--thread"), type="integer", default = 4,
+                help = "threads number to be used")
 )
 
 parser <- OptionParser(option_list = option_list,
@@ -15,10 +19,17 @@ parser <- OptionParser(option_list = option_list,
 opt <- parse_args(parser)
 
 suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(GenomicRanges))
 suppressPackageStartupMessages(library(GenomicFeatures))
 suppressPackageStartupMessages(library(rtracklayer))
 suppressPackageStartupMessages(library(BSgenome))
+suppressPackageStartupMessages(library(foreach))
+suppressPackageStartupMessages(library(doFuture))
 
+registerDoFuture()
+plan(multisession, workers = opt$thread)
+
+options(future.globals.maxSize= 2*1024*1024^2)
 
 txdb <- makeTxDbFromGFF(opt$input)
 genome <- readDNAStringSet(opt$genome)
@@ -43,18 +54,37 @@ pasteSeqs <- function(exonSeqs, intronSeqs, strand){
     return(outSeq)
 }
 
-outSeqList <- list()
 
-for(i in 1:length(exonSeqs)){
-    strand <- strand(exons[[i]]) %>%
+outSeqList <- foreach(i=1:length(exonSeqs)) %dopar% {
+    strand <- GenomicRanges::strand(exons[[i]]) %>%
         as.character() %>% unique()
-    outSeqList[i] <- pasteSeqs(exonSeqs[[i]], intronSeqs[[i]],
-                           strand)
+    pasteSeqs(exonSeqs[[i]],
+              intronSeqs[[i]],
+              strand)
 }
 
 names(outSeqList) <- names(exons)
 
-for(i in names(outSeqList)){
-    cat(paste(i, outSeqList[[i]], sep="\t"), "\n")
+format_seq <- function(seq, width=80){
+    seq <- as.character(seq) %>%
+        strsplit(split = "") %>%
+        unlist
+    outSeq <- foreach(i=seq(from=1, to=length(seq), by=width), .combine=c) %do% {
+        seq[i:(i+width-1)] %>%
+            na.omit %>%
+            paste(collapse = "")
+    }
+    paste(outSeq, collapse = "\n")
 }
 
+if(opt$outfmt == "tabular"){
+    for(i in names(outSeqList)){
+        cat(paste(i, outSeqList[[i]], sep="\t"), "\n")
+    }
+}else if(opt$outfmt == "fasta"){
+    for(i in names(outSeqList)){
+        cat(paste0(">", i, "\n", format_seq(outSeqList[[i]])), "\n")
+    }
+}else{
+    stop("Output format only support tabular or fasta.")
+}
